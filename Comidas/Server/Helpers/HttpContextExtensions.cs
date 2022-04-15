58fc89b4f12c9acf8ceb955df649fc3c0bf77397
Jesus Roso <jr8campeones@gmail.com>
2022-04-15 14:32:14 -0500

    Se realiza la parte de la paginacion

---
+++ Comidas/Server/Helpers/HttpContextExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Comidas.Server.Helpers
+{
+    public static class HttpContextExtensions
+    {
+        public async static Task InsertarParametrosPaginacionEnRespuesta<T>(this HttpContext context,
+                                                                            IQueryable<T> queryable, int cantidadRegistrosMostrar) //se va a encargar de insertar la información de paginación en la cabecera de una respuesta http                                                                          
+        {
+            if (context == null) { throw new ArgumentNullException(nameof(context)); }
+            double conteo = await queryable.CountAsync(); //con esto se hace un conteo de todos los registros de la tabla
+            double totalPaginas = Math.Ceiling(conteo / cantidadRegistrosMostrar);
+            context.Response.Headers.Add("conteo", conteo.ToString());
+            context.Response.Headers.Add("totalPaginas", totalPaginas.ToString());
+        }
+    }
+}
